b9e45d0b821680e423fea9240d10d8e64f693288
Christian Rehn <mail@christian-rehn.de>
2016-07-16 11:23:44 +0200

    initial checkin

---
+++ src/main/java/com/github/crehn/pantarhei/boundary/RestApplication.java
@@ -0,0 +1,8 @@
+package com.github.crehn.pantarhei.boundary;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+}
