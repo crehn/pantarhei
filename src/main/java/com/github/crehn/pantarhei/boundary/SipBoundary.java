cee7d0cc2ebfa8010bacfaea91ce861f8c8791f4
Christian Rehn <mail@christian-rehn.de>
2016-07-17 16:22:16 +0200

    removed swagger for now

--- src/main/java/com/github/crehn/pantarhei/boundary/SipBoundary.java
+++ src/main/java/com/github/crehn/pantarhei/boundary/SipBoundary.java
@@ -10,10 +10,6 @@ import com.github.crehn.pantarhei.control.SipFacade;
 import com.github.t1.log.LogLevel;
 import com.github.t1.log.Logged;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
-@Api(basePath = "rest")
 @Path("/sips")
 @Logged(level = LogLevel.INFO)
 public class SipBoundary {
@@ -23,14 +19,12 @@ public class SipBoundary {
 
 	@GET
 	@Path("/{guid}")
-	@ApiOperation("gets a specific sip")
 	public Sip getSip(@PathParam("guid") UUID guid) {
 		return facade.getSip(guid);
 	}
 
 	@PUT
 	@Path("/{guid}")
-	@ApiOperation("adds or replaces a sip")
 	public void putSip(@PathParam("guid") UUID guid, Sip sip) {
 		if (!guid.equals(sip.getGuid()))
 			throw new IllegalArgumentException("guid in path must match guid in sip");
