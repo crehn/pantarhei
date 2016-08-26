bba2912412f350b2f3c7503d9f3142c3d4d0846b
Christian Rehn <mail@christian-rehn.de>
2016-08-26 21:43:06 +0200

    fixed NPE

--- src/main/java/com/github/crehn/pantarhei/data/InstantConverter.java
+++ src/main/java/com/github/crehn/pantarhei/data/InstantConverter.java
@@ -11,11 +11,15 @@ public class InstantConverter implements AttributeConverter<Instant, Date> {
 
     @Override
     public Date convertToDatabaseColumn(Instant instant) {
+        if (instant == null)
+            return null;
         return Date.from(instant);
     }
 
     @Override
     public Instant convertToEntityAttribute(Date value) {
+        if (value == null)
+            return null;
         return value.toInstant();
     }
 }
